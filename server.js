ee6dba20b06416b588d9d034ed059b29893896b4
raghad-smth <143431529+raghad-smth@users.noreply.github.com>
2024-09-28 18:54:06 +0300

    uploading

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ app.get("/contact", (req, res) => {
   res.sendFile(path.join(__dirname, "contact.html"));
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
