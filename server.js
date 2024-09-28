065ffe1d9600fb42ee3b64b4abb6e0ef17740c9a
raghad-smth <143431529+raghad-smth@users.noreply.github.com>
2024-09-29 00:26:31 +0300

    test

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ const knex = require("knex")({
   },
 });
 
+
+//,
+  // "devDependencies": {
+  //   "nodemon": "^3.1.4"
+  // }
 app.use(express.static(path.join(__dirname, "Public")));
 
 app.get("/", (req, res) => {
